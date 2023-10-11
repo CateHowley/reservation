0a0db498f35adce4c764be476197efa901dcd78f
catehowley <Catherine_howley25@milton.edu>
2023-10-11 10:56:44 -0400

    Initial commit

---
+++ src/Reservation.java
@@ -0,0 +1,32 @@
+
+public class Reservation {
+
+    public int numPeople;
+
+    public double time;
+
+    int tableNumber;
+
+    public String name;
+
+    public int priority;
+
+
+    public Reservation(int tNumpeople, double tTime, int tPriority, String tName, int tTimemade) {
+        numPeople = tNumpeople;
+        time = tTime;
+        name = tName;
+        priority = tPriority;
+        tableNumber = tTimemade;
+
+    }
+
+    public void print() {
+
+
+        System.out.println(name + "booked reservation for" + numPeople + "people at" + time + " o'clock for table" + tableNumber + ".");
+
+
+    }
+}
+
---
+++ src/ReservationSystem.java
@@ -0,0 +1,16 @@
+public class ReservationSystem {
+    public static void main(String[] args) {
+        ReservationSystem jarvis = new ReservationSystem();
+    }
+
+
+    public ReservationSystem() {
+        Reservation gavin = new Reservation( 1, 4.00, 2, 567);
+        gavin.toString();
+
+
+    }
+
+}
+
+
