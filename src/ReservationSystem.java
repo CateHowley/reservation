4cc6007c4190299e49b13bcf019aec1a08a36e8c
catehowley <Catherine_howley25@milton.edu>
2023-10-13 13:27:55 -0400

    added addreservation and displayreservation method

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -4,13 +4,47 @@ public class ReservationSystem {
     }
 
 
+
+
     public ReservationSystem() {
-        Reservation gavin = new Reservation( 1, 4.00, 2, 567);
+        Reservation gavin = new Reservation( 1, 4.00, 2, "jessie", 645);
         gavin.toString();
 
 
+        Reservation dog = new Reservation(4, 8.00, 1,"jessie", 345 );
+        dog.print();
+
+        Reservation [] reservations = new Reservation [10];
+        reservations [0] = dog;
+
+        reservations [1] = gavin;
+
+        reservations [1].print();
+
+        Reservation cat = new Reservation(2, 8.00, 1, "cat", 90);
+
+
+        reservations[2] = new Reservation(2, 8.00, 1, "cat", 90);
+        reservations[2].print();
+        for(int x = 3; x<10; x++){
+            reservations [x] = new Reservation(7, 7.00, 2, "howley", 6);
+            reservations [x].print();
+            Sytem.out.println
+        }
+
+        public addReservation(){
+            System.out.println(1, 4.0, 6, "yes", 4) ;
+        }
+
+         public    displayReservation();
+                 System.out.println(1, 3.0, 1, "cate", 4) ;
+
+
+
+        }
+
     }
 
-}
+
 
 
