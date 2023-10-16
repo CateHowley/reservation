77e373b9ef0ed1aaae999421ce2e8c58f216fa08
catehowley <Catherine_howley25@milton.edu>
2023-10-15 21:30:33 -0400

    added an if statement to the displayReservation method

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -4,44 +4,58 @@ public class ReservationSystem {
     }
 
 
-
-
     public ReservationSystem() {
-        Reservation gavin = new Reservation( 1, 4.00, 2, "jessie", 645);
-        gavin.toString();
+        //  Reservation gavin = new Reservation( 1, 4.00, 2, "jessie", 645);
+        //  gavin.toString();
 
 
-        Reservation dog = new Reservation(4, 8.00, 1,"jessie", 345 );
-        dog.print();
+        //  Reservation dog = new Reservation(4, 8.00, 1,"jessie", 345 );
+        //  dog.print();
 
-        Reservation [] reservations = new Reservation [10];
-        reservations [0] = dog;
+        // Reservation [] reservations = new Reservation [10];
+        //   reservations [0] = dog;
 
-        reservations [1] = gavin;
+        //   reservations [1] = gavin;
 
-        reservations [1].print();
+        //  reservations [1].print();
 
-        Reservation cat = new Reservation(2, 8.00, 1, "cat", 90);
+        //   Reservation cat = new Reservation(2, 8.00, 1, "cat", 90);
 
 
-        reservations[2] = new Reservation(2, 8.00, 1, "cat", 90);
-        reservations[2].print();
-        for(int x = 3; x<10; x++){
-            reservations [x] = new Reservation(7, 7.00, 2, "howley", 6);
-            reservations [x].print();
-            Sytem.out.println
+        // reservations[2] = new Reservation(2, 8.00, 1, "cat", 90);
+        //  reservations[2].print();
+        //  for(int x = 3; x<10; x++){
+        //    reservations [x] = new Reservation(7, 7.00, 2, "howley", 6);
+        //   reservations [x].print();
+    }
+
+
+    public void addReservation(int tNumpeople, double tTime, int tPriority, String tName, int tTimemade) {
+        Reservation chair = new Reservation(tNumpeople, tTime, tPriority, tName, tTimemade);
+        for (int x = 0; x < reservations.length; x++) {
+            if (reservations[x] == null) {
+                System.out.println("empty");
+                reservations[x] = chair;
+                reservations(x).println();
+                break;
+
+            }
+
         }
-
-        public addReservation(){
-            System.out.println(1, 4.0, 6, "yes", 4) ;
+        public void displayReservation() {
+            Reservation display = new Reservation(tNumpeople, tTime, tPriority, tName, tTimemade);
         }
-
-         public    displayReservation();
-                 System.out.println(1, 3.0, 1, "cate", 4) ;
-
-
+        System.out.println(reservations[2]);
+        for (int y = 0; y < reservations.length; y++) {
+        if (reservations[y] == null) {
+            System.out.println("none");
+            reservations[y] = display;
+            reservations(y).println();
+            break;
 
         }
+    }
+}
 
     }
 
